7a29428b15366293323a8bb28c786a34104d2225
NikaVisenjak <nikica.visenjak@gmail.com>
2025-01-15 19:48:52 +0100

    dodani changi

--- 3DprojektVisual/Assets/3Dmodels/ON_OFF_led.cs
+++ 3DprojektVisual/Assets/3Dmodels/ON_OFF_led.cs
@@ -1,45 +1,67 @@
-using System.Collections;
-using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
+using System.Threading;
 using UnityEngine;
+
 
 public class ON_OFF_led : MonoBehaviour
 {
-    [SerializeField] private Material glowMaterial;  // Material, ki ga bomo spreminjali
-    [SerializeField] private Renderer objectToGlow;  // Objekt, ki ima material
+    [SerializeField] private Material glowMaterial;
+    [SerializeField] private Renderer objectToGlow;
 
-    private bool isEmissionOn = false;  // Spremlja stanje emisije
+    private bool isEmissionOn = false;
+    private UdpClient udpClient;
 
-    private void Update()
+    private void Start()
     {
-        // Preverite, ali je tipka 'E' pritisnjena
-        if (Input.GetKeyDown(KeyCode.E))
+        udpClient = new UdpClient(22345);
+        Thread udpThread = new Thread(ReceiveUdpMessages);
+        udpThread.IsBackground = true;
+        udpThread.Start();
+    }
+
+    private void ReceiveUdpMessages()
+    {
+        IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 22345);
+
+        try
         {
-            // Preklop emisije ob pritisku na 'E'
-            if (isEmissionOn)
+            while (true)
             {
-                turnEmissionOff();
+                byte[] data = udpClient.Receive(ref remoteEndPoint);
+                string message = Encoding.UTF8.GetString(data).Trim();
+
+                if (message == "START")
+                {
+                    MainThreadExecutor.ExecuteOnMainThread(turnEmissionOn);
+                }
+                else if (message == "STOP")
+                {
+                    MainThreadExecutor.ExecuteOnMainThread(turnEmissionOff);
+                }
             }
-            else
-            {
-                turnEmissionOn();
-            }
+        }
+        catch (SocketException e)
+        {
+            Debug.LogError("UDP Socket error: " + e.Message);
         }
     }
 
-    // Funkcija za izklop emisije
-    public void turnEmissionOff()
-    {
-        glowMaterial.DisableKeyword("_EMISSION");
-        isEmissionOn = false;
-    }
-
-    // Funkcija za vklop emisije
     public void turnEmissionOn()
     {
         glowMaterial.EnableKeyword("_EMISSION");
         isEmissionOn = true;
     }
 
+    public void turnEmissionOff()
+    {
+        glowMaterial.DisableKeyword("_EMISSION");
+        isEmissionOn = false;
+    }
+
+    private void OnApplicationQuit()
+    {
+        udpClient.Close();
+    }
 }
